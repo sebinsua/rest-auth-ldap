06eb247c811969891c8087ea88ca19173e264bed
Seb Insua <me@sebinsua.com>
2015-05-19 15:24:32 +0100

    Wipe the old name.

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var NODE_ENV = process.env.NODE_ENV || 'development',
 var app = require('./app/configure')(NODE_ENV, config),
     logger = app.get('logger');
 
-logger.info('Starting rest-examiner-psn...');
+logger.info('Starting rest-auth-ldap...');
 app.listen(app.get('port'), function () {
     logger.info('Listening for requests on port ' + app.get('port') + '...');
 });
