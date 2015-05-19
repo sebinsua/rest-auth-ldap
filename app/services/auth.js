82d048456e5bdb9be7a589565b517162edc42b99
Seb Insua <me@sebinsua.com>
2015-05-19 18:09:05 +0100

    TODO.

--- app/services/auth.js
+++ app/services/auth.js
@@ -4,6 +4,8 @@
 
 var LdapService = require('./ldap');
 
+var jwt = require('jsonwebtoken');
+
 var config = require('../../config');
 
 // var debug = require('debug')('rest-auth-ldap:services:auth');
@@ -19,6 +21,22 @@ AuthService.prototype.getAuthToken = function (username, password) {
   });
 
   var authenticateUser = ldapService.authenticate(username, password);
+
+  // TODO: Use a private key instead of a shared secret.
+  //       - The PEM encoded private key for RSA and ECDSA.
+  //       See: http://stackoverflow.com/questions/5244129/use-rsa-private-key-to-generate-public-key
+  //       Check to see if webpack can support crypto. Perhaps use browserify-crypto
+  // TODO: Set the audience, subject and issuer.
+  //       See: http://self-issued.info/docs/draft-ietf-oauth-json-web-token.html#issDef
+  // TODO: payload should be an object.
+  // TODO: Create promises instead of using callbacks.
+
+  var token = jwt.sign('some-data', 'some-secret');
+  var found = jwt.verify(token, 'some-secret-fake');
+  var and = jwt.decode(token, { complete: true });
+  console.log(token);
+  console.log(found);
+  console.log(and);
 
   return authenticateUser;
 };
